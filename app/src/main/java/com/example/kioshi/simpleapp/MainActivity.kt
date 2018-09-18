d024d236e9e2e8c05aa01c3feadbf086e0c6a131
Ivan <kevichivan@gmail.com>
2018-09-19 00:23:13 +0300

    Added color Support

--- app/src/main/java/com/example/kioshi/simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/kioshi/simpleapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.kioshi.simpleapp
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +15,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun toastMe(view: View){
-        // val myToast = Toast.makeText(this, message, duration);
-        val myToast = Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT)
-        myToast.show()
+        button3.setOnClickListener {
+            button3.setBackgroundColor(Color.RED)
+            val random = Random()
+
+            var color: Int = Color.argb(255, random.nextInt(256),random.nextInt(256),random.nextInt(256))
+            button3.setBackgroundColor(color)
+
+            val myToast = Toast.makeText(this, "color changed! >_<", (Toast.LENGTH_SHORT)/2).show()
+        }
     }
 
     fun countMe(view: View){
